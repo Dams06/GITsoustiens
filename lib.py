7d0236b7192067462b2cfc045aeb6163821a0965
Dams <damien.labranche@ynov.com>
2019-01-22 15:39:49 +0100

    coucou

--- lib.py
+++ lib.py
@@ -1,10 +1,5 @@
 def Dab():
     print("Hello world")
-<<<<<<< HEAD
-def EHOUAIS():
-    print("T'Y croyais pas hein")
-=======
 
 def lol():
     print("bite")
->>>>>>> 3b6c9a329abbfcce584027800af7a9ed53ad5508
